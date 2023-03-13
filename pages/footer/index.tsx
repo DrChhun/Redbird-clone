d0d20c560491699535635156c2aebd7e3d6ada89
chhunkim394 <chhunkim394@gmail.com>
2023-03-13 10:11:46 +0700

    Fixed: spacing bug

--- pages/footer/index.tsx
+++ pages/footer/index.tsx
@@ -7,12 +7,12 @@ function Footer() {
             <div className="py-12 px-10 bg-[#ed1c24] text-white">
                 <div className="md:grid md:grid-cols-3">
                     <div className="text-center md:text-left mb-8 md:mb-0">
-                        <h1 className="text-lg font-bold">ស្វែងយល់អំពីពួកយើង</h1>
+                        <h1 className="text-base md:text-lg font-bold">ស្វែងយល់អំពីពួកយើង</h1>
                         <p className="py-4">អំពីពួកយើង</p>
                         <p>sam.businesscambodia@gmail.com</p>
                     </div>
                     <div className="text-center mb-8 md:mb-0">
-                        <h1 className="text-lg mb-4">អត្ថបទផ្សេងៗ</h1>
+                        <h1 className="text-base md:text-lg mb-4">អត្ថបទផ្សេងៗ</h1>
                         <div className="flex justify-center">
                             <ul className="">
                                 <li>១</li>
@@ -31,9 +31,9 @@ function Footer() {
                         </div>
                     </div>
                     <div className="text-center md:text-right">
-                        <h1 className="text-lg">ទំនាក់ទំនងពួកយើង</h1>
-                        <p className="font-light py-4">017 300 755/093 300 755</p>
-                        <p className="font-light">Building 422 St. 271, Phum 1, Stung Mean Chey</p>
+                        <h1 className="text-base md:text-lg">ទំនាក់ទំនងពួកយើង</h1>
+                        <p className="text-xs md:text-base ont-light py-4">017 300 755/093 300 755</p>
+                        <p className="text-xs md:text-base font-light">Building 422 St. 271, Phum 1, Stung Mean Chey</p>
                     </div>
                 </div>
                 <div className="text-center">
@@ -43,8 +43,8 @@ function Footer() {
                 </div>
             </div>
             <div className="text-center md:flex md:justify-between py-6 px-10 font-light">
-                <h1>@2022 Baksey Media All Right Reserved</h1>
-                <a className="group" href="#">Developed by: <span className="group-hover:underline">ChunMEGA</span></a>
+                <h1 className="text-xs md:text-base">@2022 Baksey Media All Right Reserved</h1>
+                <a className="group text-xs md:text-base" href="#">Developed by: <span className="group-hover:underline">ChunMEGA</span></a>
             </div>
         </>
     )
