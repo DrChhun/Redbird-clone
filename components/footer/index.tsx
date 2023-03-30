5945b446077724b48d804eff34e1345e14bb1f3e
chhunkim394 <chhunkim394@gmail.com>
2023-03-30 16:13:12 +0700

    fixed h1 in home page

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -10,12 +10,12 @@ function Footer() {
             <div className="py-12 px-10 bg-[#ed1c24] text-white">
                 <div className="md:grid md:grid-cols-3">
                     <div className="text-center md:text-left mb-8 md:mb-0">
-                        <h1 className="text-xs md:text-lg font-bold">ស្វែងយល់អំពីពួកយើង</h1>
+                        <h2 className="text-xs md:text-lg font-bold">ស្វែងយល់អំពីពួកយើង</h2>
                         <p className="py-4 text-xs lg:text-base">អំពីពួកយើង</p>
                         <a href="mailto: sam.businesscambodia@gmail.com" className="text-xs text:base">sam.businesscambodia@gmail.com</a>
                     </div>
                     <div className="text-center mb-8 md:mb-0">
-                        <h1 className="text-xs font-bold md:text-lg mb-4">អត្ថបទផ្សេងៗ</h1>
+                        <h2 className="text-xs font-bold md:text-lg mb-4">អត្ថបទផ្សេងៗ</h2>
                         <div className="flex justify-center">
                             <ul className="text-start mx-4 text-xs lg:text-base">
                                 <li>
@@ -46,7 +46,7 @@ function Footer() {
                         </div>
                     </div>
                     <div className="text-center md:text-right">
-                        <h1 className="text-xs font-bold md:text-lg">ទំនាក់ទំនងពួកយើង</h1>
+                        <h2 className="text-xs font-bold md:text-lg">ទំនាក់ទំនងពួកយើង</h2>
                         <a href="tel: 017300755" className="text-xs md:text-base ont-light py-4">017 300 755 / </a>
                         <a href="tel: 093300755">093 300 755</a> <br />
                         <a target="_blank" href="https://goo.gl/maps/VFp3DXYc82R1s2Kq6" className="text-xs md:text-base font-light">Building 422 St. 271, Phum 1, Stung Mean Chey</a>
@@ -54,12 +54,12 @@ function Footer() {
                 </div>
                 <div className="text-center">
                     <div className="h-32 w-32"></div>
-                    <h1 className="font-bold">Business Camboida</h1>
+                    <h1 className="font-bold">Redbird News</h1>
                     <p className="text-xs lg:text-base">បំផុសគំនិតរកសុី នាំមនុស្សអោយហ៊ានចេញរកសុីតាមក្តីស្រមៃ</p>
                 </div>
             </div>
             <div className="text-center md:flex md:justify-between py-6 px-10 font-light">
-                <h1 className="text-xs md:text-base">@2022 Baksey Media All Right Reserved</h1>
+                <h2 className="text-xs md:text-base">@2022 Baksey Media All Right Reserved</h2>
                 <a className="group text-xs md:text-base" href="#">Developed by: <span className="group-hover:underline">ChunMEGA</span></a>
             </div>
         </>
